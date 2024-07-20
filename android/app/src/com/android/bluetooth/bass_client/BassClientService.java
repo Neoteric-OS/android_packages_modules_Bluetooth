954e0e85f1fac87e13e0c002382bd3bbf5d2b57d
zhenchao <quic_zhenchao@quicinc.com>
2024-07-20 20:56:13 +0800

    BASS: use broadcastId instead of adv Sid to check for local source
    Issue: non-collocated source can not be added while local source
    is enabled but not added.
    
    Steps:
    1. Pair and connect LeAudio earbuds
    2. Play music and enable broadcast
    3. Go to find broadcasts and remove added collocated source
    4. Add non collocated source listed in find broadcasts UI
    
    Root cause: isLocalBroadcast return true for non-collocated source,
    which prevent source to be added.
    
    Fix: Use broadcastId instead of adv Sid to check for local source.
    
    CRs-Fixed: 3873581
    Change-Id: I5876981a8cad542feebbce4b9fcf7468b7a8ffc0

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -2795,7 +2795,7 @@ public class BassClientService extends ProfileService {
         return stateMachine.getMaximumSourceCapacity();
     }
 
-    private boolean isLocalBroadcast(int sourceAdvertisingSid) {
+    private boolean isLocalBroadcast(int broadcastId) {
         LeAudioService leAudioService = mServiceFactory.getLeAudioService();
         if (leAudioService == null) {
             return false;
@@ -2805,7 +2805,7 @@ public class BassClientService extends ProfileService {
                 leAudioService.getAllBroadcastMetadata().stream()
                         .anyMatch(
                                 meta -> {
-                                    return meta.getSourceAdvertisingSid() == sourceAdvertisingSid;
+                                    return meta.getBroadcastId() == broadcastId;
                                 });
         log("isLocalBroadcast=" + wasFound);
         return wasFound;
@@ -2816,7 +2816,7 @@ public class BassClientService extends ProfileService {
             return false;
         }
 
-        return isLocalBroadcast(metaData.getSourceAdvertisingSid());
+        return isLocalBroadcast(metaData.getBroadcastId());
     }
 
     boolean isLocalBroadcast(BluetoothLeBroadcastReceiveState receiveState) {
@@ -2824,7 +2824,7 @@ public class BassClientService extends ProfileService {
             return false;
         }
 
-        return isLocalBroadcast(receiveState.getSourceAdvertisingSid());
+        return isLocalBroadcast(receiveState.getBroadcastId());
     }
 
     static void log(String msg) {
