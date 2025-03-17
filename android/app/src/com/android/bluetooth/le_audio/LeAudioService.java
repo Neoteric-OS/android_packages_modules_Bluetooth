57ac4a4d7ca5ca194eda7e7cb049a8d7c1cb1922
zhenchao <quic_zhenchao@quicinc.com>
2025-03-17 16:44:00 +0800

    LE audio: allow to set le audio device as active while Broadcast enabled
    If leaudioBigDependsOnAudioState is true, allow to set le audio device
    as active while Broadcast enabled.
    
    CRs-Fixed: 4086155
    Change-Id: Ibaa6576859a3f9518df27122520dece685789c5b

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -3881,9 +3881,11 @@ public class LeAudioService extends ProfileService {
                     {
                         handleGroupTransitToActive(groupId);
 
-                        /* Clear possible exposed broadcast device after activating unicast */
-                        if (mActiveBroadcastAudioDevice != null) {
-                            updateBroadcastActiveDevice(null, mActiveBroadcastAudioDevice, true);
+                        if (!leaudioBigDependsOnAudioState()) {
+                            /* Clear possible exposed broadcast device after activating unicast */
+                            if (mActiveBroadcastAudioDevice != null) {
+                                updateBroadcastActiveDevice(null, mActiveBroadcastAudioDevice, true);
+                            }
                         }
                         break;
                     }
