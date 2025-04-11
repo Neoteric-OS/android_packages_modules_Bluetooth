67974df4f06bcfae4505793da8fa64715ec3058e
Katta Karthik Reddy <quic_kattkart@quicinc.com>
2025-04-11 14:09:20 +0530

    GATT: Adding interop to skip GATT discovery.
    Root cause: Some remote devices initiate SMP over BR, which triggers
    btif_dm_ble_auth_cmpl_evt and starts GATT service discovery. This flow
    blocks pairing_cb reset, preventing subsequent pairings with other
    devices.
    
    CRs-Fixed: 4377614
    Change-Id: Iff4a1f810ec862e1741312368a5a550f5a42170d

--- system/device/include/interop.h
+++ system/device/include/interop.h
@@ -393,6 +393,8 @@ typedef enum {
   // disables stack's 'wait for LE service discovery' logic if LE advertising cannot be established.
   INTEROP_SKIP_WAIT_FOR_LE_SERVICE_SEARCH,
 
+  INTEROP_SKIP_GATT_DISCOVERY_OVER_LE,
+
   END_OF_INTEROP_LIST
 } interop_feature_t;
 
