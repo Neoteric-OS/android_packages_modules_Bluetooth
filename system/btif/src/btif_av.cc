6ec58f889e67bfddd9e305f00781de7deb965871
Pranshu Bansal <pranbans@qti.qualcomm.com>
2026-01-22 17:13:16 +0530

    Issue: For A2dp Sink offload usecase, connection state was being reported multiple times.
    Fix: Put a check to avoid reporting connection state for DUT acting as
    Sink.
    
    CRs-Fixed: 4411689
    Change-Id: I27f15f0f046a1d149b6826becf116635f25d4027

--- system/btif/src/btif_av.cc
+++ system/btif/src/btif_av.cc
@@ -2126,13 +2126,16 @@ bool BtifAvStateMachine::StateIdle::ProcessEvent(uint32_t event, void* p_data) {
                                        peer_.IsSource() ? A2dpType::kSink : A2dpType::kSource);
           peer_.StateMachine().TransitionTo(BtifAvStateMachine::kStateIdle);
         } else {
-          if (peer_.IsSource() && (p_bta_data->open.status == BTA_AV_SUCCESS)) {
+          /* Report connection state for DUT acting as Source.
+             For DUT acting as Sink, connection state reporting handled in opened state. */
+          if (peer_.IsSink()) {
+            btif_report_connection_state(peer_.PeerAddress(), BTAV_CONNECTION_STATE_CONNECTED,
+                                         bt_status_t::BT_STATUS_SUCCESS, BTA_AV_SUCCESS,
+                                         A2dpType::kSource);
+          } else if (peer_.IsSource()) {
             // Bring up AVRCP connection as well
             BTA_AvOpenRc(peer_.BtaHandle());
           }
-          btif_report_connection_state(peer_.PeerAddress(), BTAV_CONNECTION_STATE_CONNECTED,
-                                       bt_status_t::BT_STATUS_SUCCESS, BTA_AV_SUCCESS,
-                                       peer_.IsSource() ? A2dpType::kSink : A2dpType::kSource);
           peer_.StateMachine().TransitionTo(BtifAvStateMachine::kStateOpened);
         }
       } else {
