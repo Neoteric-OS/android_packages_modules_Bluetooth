db41c12fc42154736781bf8f64b70e2a05016c97
asirise <asirise@qti.qualcomm.com>
2026-02-23 19:08:48 +0530

    Fix: Send metadata context update after group activation
    Previously, the metadata context was shared before the group became
    active, which resulted in the preferred profile being set for the
    incorrect device. This change ensures the context update occurs only
    after the group is active, correctly targeting the intended device.
    
    CRs-Fixed: 4414952
    Change-Id: I93c572943456b2713272cb643d16c4855737ba57

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -104,6 +104,10 @@
 #include <hardware/audio.h>
 #endif  // TARGET_FLOSS
 
+#ifdef __ANDROID__
+#include "os/system_properties.h"
+#endif
+
 using namespace bluetooth;
 using base::Closure;
 using bluetooth::Uuid;
@@ -1444,6 +1448,17 @@ public:
     SendAudioGroupSelectableCodecConfigChanged(group);
     SendAudioGroupCurrentCodecConfigChanged(group);
     callbacks_->OnGroupStatus(active_group_id_, GroupStatus::ACTIVE);
+
+    /* Notify metadata update for dual mode audio profile selection */
+#ifdef __ANDROID__
+    if (bluetooth::os::GetSystemPropertyBool(
+                  bluetooth::os::kIsDualModeAudioEnabledProperty, false)) {
+      uint16_t context_update_ = LeAudioContextToIntContent(configuration_context_type_);
+      log::info("OnMetadataUpdate for context type: {} when device became active",
+                    ToHexString(configuration_context_type_));
+      callbacks_->OnMetadataUpdate(context_update_);
+    }
+#endif
   }
 
   void CheckAndNotifyGroupInactive(const int group_id) {
